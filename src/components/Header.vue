df1277a786e641e9447b8a8f61d5ad5c8eb016c2
elisejakob <hei@elisejakob.no>
2019-01-04 16:20:55 +0100

    🌶  add taglines

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="site-header">
     <Logo />
-    <div class="header-text">{{ text }}</div>
+    <div class="header-text">{{ tagline[Math.floor(Math.random() * tagline.length)] }}</div>
   </header>
 </template>
 
@@ -10,8 +10,10 @@ import Logo from '@/components/Logo.vue'
 
 export default {
   name: 'Header',
-  props: {
-    text: String
+  data: function() {
+    return {
+      tagline: ["5 om dagen", "100% vegansk", "Økologisk", "Biodynamisk", "Lokalprodusert", "Fair trade"]
+    }
   },
   components: {
     Logo
