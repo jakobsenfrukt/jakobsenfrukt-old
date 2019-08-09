157a0209e26d97935678193c7aeda93b1dc521dd
elisejakob <hei@elisejakob.no>
2019-08-09 10:26:01 +0200

    remove comment

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
 import Contact from '@/components/Contact.vue'
